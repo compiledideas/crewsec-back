2131cc57c89c88069110920868798762c32d79ce
abdenassar01 <abdenassaramimi99@gmail.com>
2024-12-24 14:21:33 +0100

    setup(global): initial commit [setup new project]

---
+++ src/main/java/com/compiledideas/crewsecback/CrewsecBackApplication.java
@@ -0,0 +1,13 @@
+package com.compiledideas.crewsecback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewsecBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewsecBackApplication.class, args);
+    }
+
+}
