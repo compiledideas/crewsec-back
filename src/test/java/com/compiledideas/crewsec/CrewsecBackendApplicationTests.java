7b4e4563bbe020aabff4f37d54e48c21bd3f9479
abdenassar01 <abdenassaramimi99@gmail.com>
2024-12-30 11:06:47 +0100

    fix(build): fix build error

--- src/test/java/com/compiledideas/crewsec/CrewsecBackendApplicationTests.java
+++ src/test/java/com/compiledideas/crewsec/CrewsecBackendApplicationTests.java
@@ -1,9 +1,12 @@
 package com.compiledideas.crewsec;
 
+import com.compiledideas.crewsecback.CrewsecBackApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = CrewsecBackApplication.class)
 class CrewsecBackendApplicationTests {
 
     @Test
